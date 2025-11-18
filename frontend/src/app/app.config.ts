954d363a089157f756f8f938415a23ff20d4a40e
Wallace Maia <wallacemaia2007@gmail.com>
2025-11-17 23:52:51 -0300

    Feat: Create Header and update PrimeNG version to 20+

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+  providers: [provideAnimationsAsync(), providePrimeNG({})],
 };
---
+++ frontend/src/app/shared/components/header-component/header-component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header-component',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './header-component.html',
+  styleUrl: './header-component.scss',
+})
+export class HeaderComponent {
+  categories = [
+    { name: 'Novidades', link: '/novidades' },
+    { name: 'Vestidos', link: '/vestidos' },
+    { name: 'Tops', link: '/tops' },
+    { name: 'Croppeds', link: '/croppeds' },
+    { name: 'Casacos', link: '/casacos' },
+    { name: 'Acessórios', link: '/acessorios' },
+    { name: 'Ofertas', link: '/ofertas' }
+  ];
+
+
+  onSearch() {
+  }
+
+  onFavorites() {
+  }
+
+  onCart() {
+  }
+}
+
