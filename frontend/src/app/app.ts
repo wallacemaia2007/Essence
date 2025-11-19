b1f18f346cdcb09d842041935e7f26071c365b0d
Wallace Maia <wallacemaia2007@gmail.com>
2025-11-19 17:12:59 -0300

    Refactor: Create layout folder for Header and Footer components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/components/header-component/header-component";
-import { FooterComponent } from './shared/components/footer-component/footer-component';
+import { HeaderComponent } from "./shared/layout/header-component/header-component";
+import { FooterComponent } from './shared/layout/footer-component/footer-component';
 
 @Component({
   selector: 'app-root',
